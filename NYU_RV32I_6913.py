be9c50da9d80d6c89ba77a4823288ca002b2bec0
HoneyBreaks <919602022@qq.com>
2022-12-03 23:41:11 -0500

    test for TC0

--- NYU_RV32I_6913.py
+++ NYU_RV32I_6913.py
@@ -5,7 +5,6 @@ import argparse
 # we keep it as this large number, but the memory is still 32-bit addressable.
 MemSize = 1000
 
-
 class InsMem(object):
     def __init__(self, name, ioDir):
         self.id = name
@@ -19,7 +18,7 @@ class InsMem(object):
         Instruction = 0
         for i in range(0, 4):  # 4 lines for an instruction
             Instruction <<= 8  # left shift
-            Instruction = Instruction + int(self.IMem[ReadAddress + i], 2)  # TODO: may need to process "ReadAddress"
+            Instruction = Instruction + int(self.IMem[ReadAddress + i], 2)
         return Instruction
 
 
@@ -36,11 +35,13 @@ class DataMem(object):
         Instruction = 0
         for i in range(0, 4):  # 4 lines for an instruction
             Instruction <<= 8  # left shift
-            Instruction = Instruction + int(self.DMem[ReadAddress + i], 2)  # TODO: may need to process "ReadAddress"
+            Instruction = Instruction + int(self.DMem[ReadAddress + i], 2)
         return Instruction
 
     def writeDataMem(self, Address, WriteData):
         # write data into byte addressable memory
+        WriteData = str(bin(WriteData))[2:]
+        WriteData = WriteData.zfill(32)
         for i in range(0, 4):  # 4 lines for an instruction
             self.DMem[Address + i] = WriteData[0 + 8 * i: 8 + 8 * i]  # WriteData is Str
 
@@ -62,11 +63,13 @@ class RegisterFile(object):
 
     def writeRF(self, Reg_addr, Wrt_reg_data):
         # Fill in
+        Reg_addr = int(Reg_addr, 2)
         self.Registers[Reg_addr] = Wrt_reg_data
 
     def outputRF(self, cycle):
         op = ["-"*70+"\n", "State of RF after executing cycle:" + str(cycle) + "\n"]
-        op.extend([str(val)+"\n" for val in self.Registers])
+        # TODO: changed "str(val)" to "[str(bin(val))[2:].zfill(32)"
+        op.extend([str(bin(val))[2:].zfill(32)+"\n" for val in self.Registers])
         if (cycle == 0): perm = "w"
         else: perm = "a"
         with open(self.outputFile, perm) as file:
@@ -96,6 +99,35 @@ class Core(object):
         self.ext_dmem = dmem
 
 
+def ALUOperation(ALUop, oprand1, oprand2):
+    oprand1 = int(oprand1, 2)
+    oprand2 = int(oprand2, 2)
+    print("oprand1: " + str(bin(oprand1))[2:].zfill(32))
+    print("oprand2: " + str(bin(oprand2))[2:].zfill(32))
+    result = 0
+    if ALUop == 0:
+        return result
+    if ALUop == 1:
+        result = oprand1 + oprand2
+    if ALUop == 2:
+        result = oprand1 - oprand2
+    if ALUop == 3:
+        result = oprand1 ^ oprand2
+    if ALUop == 4:
+        result = oprand1 or oprand2
+    if ALUop == 5:
+        result = oprand1 and oprand2
+    return result
+
+
+def signProcess(imm):
+    if imm[0] == "0":
+        str = "00000000000000000000" + imm
+    else:
+        str = "11111111111111111111" + imm
+    return str
+
+
 class SingleStageCore(Core):
     def __init__(self, ioDir, imem, dmem):
         super(SingleStageCore, self).__init__(ioDir + "\\SS_", imem, dmem)
@@ -103,9 +135,95 @@ class SingleStageCore(Core):
 
     def step(self):
         # Your implementation
-        self.halted = True
-        if self.state.IF["nop"]:
+        print("PC: " + str(self.state.IF["PC"]))
+        Instruction = imem.readInstr(self.state.IF["PC"])
+        # halted if instruction is 0xffffffff
+        if Instruction == 0xffffffff:
+            print("meet 0xffffffff")
             self.halted = True
+        else:
+            self.halted = False
+            self.nextState.IF["PC"] = self.state.IF["PC"] + 4
+
+            instr = str(bin(Instruction))[2:]
+            instr = instr.zfill(32)
+            print("instr:   " + instr)
+            opcode = instr[25:]
+
+            RType = (opcode == "0110011")
+            IType = (opcode == "0010011" or opcode == "0000011")
+            JType = (opcode == "1101111")
+            BType = (opcode == "1100011")
+            SType = (opcode == "0100011")
+
+            IsBranch = (opcode == "1100011")
+            IsLoad = (opcode == "0000011")
+            IsStore = (opcode == "0100011")
+            WrtEnable = not (IsStore or IsBranch or JType)
+
+            funct7 = instr[:7]
+            imm = instr[:12]
+            rs2 = instr[7:12]
+            rs1 = instr[12:17]
+            funct3 = instr[17:20]
+            rd = instr[20:25]
+            ALUop = 0
+
+            print("rs1: " + rs1 + " -> " + str(int(rs1, 2)))
+            print("rs2: " + rs2 + " -> " + str(int(rs2, 2)))
+
+            readData1 = self.myRF.readRF(int(rs1, 2))
+            readData2 = self.myRF.readRF(int(rs2, 2))
+
+            # EX
+            ALUin1 = str(bin(readData1))[2:]
+            if IType:
+                ALUin2 = signProcess(imm)
+            else:
+                ALUin2 = str(bin(readData2))[2:]
+
+            if RType:
+                if funct7 == "0100000": ALUop = 2
+                else:
+                    if funct3 == "000": ALUop = 1
+                    if funct3 == "100": ALUop = 3
+                    if funct3 == "110": ALUop = 4
+                    if funct3 == "111": ALUop = 5
+            if IType:
+                if opcode == "0010011":
+                    if funct3 == "000": ALUop = 1
+                    if funct3 == "100": ALUop = 3
+                    if funct3 == "110": ALUop = 4
+                    if funct3 == "111": ALUop = 5
+                if opcode == "0000011": ALUop = 1
+
+            ALUout = ALUOperation(ALUop, ALUin1, ALUin2)
+            print("ALUout:  " + str(bin(ALUout))[2:].zfill(32))
+
+            # MEM
+            DMAddr = ALUout
+            WrtData = readData2
+            ReadMem = IsLoad
+            WriteMem = IsStore
+
+            if ReadMem:
+                readData = dmem_ss.readInstr(DMAddr)
+                print("IsLoad: " + str(IsLoad))
+
+            elif WriteMem:
+                dmem_ss.writeDataMem(DMAddr, WrtData)
+                print("IsStore: " + str(IsStore))
+
+            # WB
+            if IsLoad:
+                WData = readData
+            else:
+                WData = ALUout
+
+            if WrtEnable:
+                self.myRF.writeRF(rd, WData)
+
+            print("----------------------------------------")
 
         self.myRF.outputRF(self.cycle)  # dump RF
         self.printState(self.nextState, self.cycle)  # print states after executing cycle 0, cycle 1, cycle 2 ...
@@ -150,6 +268,127 @@ class FiveStageCore(Core):
 
 
         # --------------------- IF stage ---------------------
+        # IF
+        if not self.state.IF["nop"]:
+            Instruction = imem.readInstr(self.state.IF["PC"])
+            # halted if instruction is 0xffffffff
+            if Instruction == 0xffffffff:
+                self.halted = True
+            else:
+                self.halted = False
+                self.nextState.ID["Instr"] = Instruction
+                self.nextState.IF["PC"] = self.state.IF["PC"] + 4
+
+        # if self.state.IF["nop"]:
+        #     self.halted = True
+
+        # ID
+        if not self.state.ID["nop"]:
+            instr = str(bin(self.state.ID["Instr"]))[2:]
+            opcode = instr[25:]
+
+            funct7 = ""
+            rs2 = ""
+            rs1 = ""
+            funct3 = ""
+            rd = ""
+            imm = ""
+
+            RType = (opcode == "0110011")
+            IType = (opcode == "0010011" or opcode == "0000011")
+            JType = (opcode == "1101111")
+            BType = (opcode == "1100011")
+            SType = (opcode == "0100011")
+
+            if RType:
+                funct7 = instr[:7]
+                rs2 = instr[7:12]
+                rs1 = instr[12:17]
+                funct3 = instr[17:20]
+                rd = instr[20:25]
+
+            if IType:
+                imm = instr[:12]
+                rs1 = instr[12:17]
+                funct3 = instr[17:20]
+                rd = instr[20:25]
+
+            if JType:
+                imm = instr[:20]
+                rd = instr[20:25]
+
+            if BType or SType:
+                imm = instr[:7] + instr[20:25]
+                rs2 = instr[7:12]
+                rs1 = instr[12:17]
+                funct3 = instr[17:20]
+
+            self.nextState.EX["Read_data1"] = 0
+            self.nextState.EX["Read_data2"] = 0
+            self.nextState.EX["Imm"] = int(imm, 2)
+            self.nextState.EX["Rs"] = int(rs1, 2)
+            self.nextState.EX["Rt"] = int(rs2, 2)
+            self.nextState.EX["Wrt_reg_addr"] = 0
+            self.nextState.EX["is_I_type"] = False
+            self.nextState.EX["rd_mem"] = 0
+            self.nextState.EX["wrt_mem"] = 0
+            self.nextState.EX["alu_op"] = 0
+            self.nextState.EX["wrt_enable"] = 0
+
+            res = 0
+            rnull = 0
+            if RType:
+                if funct7 == "0100000":
+                    ALUop = 0
+                    res = ALUOperation(ALUop, rs1, rs2)
+                else:
+                    if funct3 == "000":
+                        ALUop = 1
+                        res = ALUOperation(ALUop, rs1, rs2)
+                    if funct3 == "100":
+                        ALUop = 2
+                        res = ALUOperation(ALUop, rs1, rs2)
+                    if funct3 == "110":
+                        ALUop = 3
+                        res = ALUOperation(ALUop, rs1, rs2)
+                    if funct3 == "111":
+                        ALUop = 4
+                        res = ALUOperation(ALUop, rs1, rs2)
+            if IType:
+                if opcode == "0000011":
+                    ALUop = 9
+                    res = ALUOperation(ALUop, rs1, rs2)
+                else:
+                    if funct3 == "000":
+                        ALUop = 5
+                        res = ALUOperation(ALUop, rs1, imm)
+                    if funct3 == "100":
+                        ALUop = 6
+                        res = ALUOperation(ALUop, rs1, imm)
+                    if funct3 == "110":
+                        ALUop = 7
+                        res = ALUOperation(ALUop, rs1, imm)
+                    if funct3 == "111":
+                        ALUop = 8
+                        res = ALUOperation(ALUop, rs1, imm)
+            # under determined
+            # if JType:
+            #     ALUop = 9
+            #     res = ALUOperation(ALUop, rnull, imm)
+            #
+            # if BType or SType:
+            #     if funct3 == "000":
+            #         ALUop = 10
+            #         res = ALUOperation(ALUop, rs1, imm)
+            #     if funct3 == "001":
+            #         ALUop = 11
+            #         res = ALUOperation(ALUop, rs1, imm)
+            #     if funct3 == "010":
+            #         ALUop = 12
+            #         res = ALUOperation(ALUop, rs1, imm)
+
+
+
 
         self.halted = True
         if self.state.IF["nop"] and self.state.ID["nop"] and self.state.EX["nop"] and self.state.MEM["nop"] and self.state.WB["nop"]:
@@ -194,13 +433,15 @@ if __name__ == "__main__":
     while(True):
         if not ssCore.halted:
             ssCore.step()
+        # if not fsCore.halted:
+        #     print(fsCore.halted)
+        #     fsCore.step()
+        # if ssCore.halted and fsCore.halted:
+        #     break
 
-        if not fsCore.halted:
-            fsCore.step()
-
-        if ssCore.halted and fsCore.halted:
+        if ssCore.halted:
             break
 
     # dump SS and FS data mem.
     dmem_ss.outputDataMem()
-    dmem_fs.outputDataMem()
+    # dmem_fs.outputDataMem()
